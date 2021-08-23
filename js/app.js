4568a2005783c565bd77805b021ed3b5fb448116
Luiz Paulo <luizpaulopds@outlook.com.br>
2021-08-23 03:16:03 -0300

    JavaScript Básico

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// > 01 - Chama o valor da tag pelo id
+// console.log(
+//     document.getElementById("titulo").innerHTML
+// );
+
+
+
+// > 02 - Retorna a largura da tela
+// console.log(
+//     window.screen.width
+// );
+
+// Retorna no console os objetos da janela do navegador (BOM)
+// console.log(
+//     window
+// );
+
+// Retorna no console os objetos do docmento HTML (DOM)
+// console.log(
+//     window.document
+// );
+
+
+
+// > 03 - Valores que são retornados da janela do navegador (BOM)
+// console.log(
+//     screen.width,
+//     screen.height,
+//     screen.orientation,
+//     screen.orientation.type
+// );
+
+// Retorna informações de tela. Incluir o '.' e o método para retornar um valor específico
+// console.log(
+//     screen
+// );
+
+// Retorna as informações do navegador. Incluir o '.' e o método para retornar um valor específico
+// console.log(
+//     navigator
+//     // navigator.language
+// );
+
+// Retorna a localização do navegador
+// console.log(
+//     location
+// );
+
+// Redireciona para um link
+// location.href = "http://www.google.com"
+
+// Back e next com history
+// function voltar() {
+//     history.back()
+// }
+// function adiantar() {
+//     history.forward()
+// }
+
+// Retorna javascript no html com .innerHTML
+// document.getElementById("largura").innerHTML = screen.width;
+// document.getElementById("altura").innerHTML = screen.height;
+// document.getElementById("orientation").innerHTML = screen.orientation.type;
